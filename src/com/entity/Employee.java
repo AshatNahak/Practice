8eca0a7ba5ee883fea16c95441fc5dc9f2256448
a913148 <a913148@192.168.0.7>
2024-03-20 02:32:46 +0530

    After stash

--- src/com/entity/Employee.java
+++ src/com/entity/Employee.java
@@ -3,7 +3,11 @@ package com.entity;
 public class Employee {
 	private int id;
 	private String name;
+
 	private Double salary1;
 	private String company;
+
+	private String DOB;
+
 
 }
--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
 		System.out.println("Hii, Sam");
+
+		System.out.println("Hii,Bharat");
+
 		System.out.println("hello");
 		//hey how are you
 		//checking for stash
