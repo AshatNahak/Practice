8ebed391aacab47499c8d4d46fa71512faa7844a
AshatNahak <164081776+AshatNahak@users.noreply.github.com>
2024-03-20 02:26:20 +0530

    added sam

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hii");
+		System.out.println("Hii, Sam");
 		System.out.println("hello");
 		//hey how are you
 		//checking for stash
