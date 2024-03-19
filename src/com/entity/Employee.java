4cf2c910ba9a9f0736e342205f867df4a921138f
a913148 <a913148@192.168.0.7>
2024-03-20 02:41:31 +0530

    added createDate in entity

--- src/com/entity/Employee.java
+++ src/com/entity/Employee.java
@@ -1,11 +1,14 @@
 package com.entity;
 
+import java.util.Date;
+
 public class Employee {
 	private int id;
 	private String name;
 	private Double salary1;
 	private String company;
 	private String DOB;
+	private Date createDate;
 
 
 }
