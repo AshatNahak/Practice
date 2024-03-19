62122332668c5271b544bfed9e294a6275bfee93
a913148 <a913148@192.168.0.7>
2024-03-20 02:58:24 +0530

    removed salary

--- src/com/entity/Employee.java
+++ src/com/entity/Employee.java
@@ -5,10 +5,10 @@ import java.util.Date;
 public class Employee {
 	private int id;
 	private String name;
-	private Double salary1;
 	private String company;
 	private String DOB;
 	private Date createDate;
+	private Date updateDate;
 
 
 }
